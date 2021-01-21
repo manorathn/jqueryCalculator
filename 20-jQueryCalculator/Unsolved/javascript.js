93547092f9d5f2d057bd9a5fc460bb563b4eba68
Gerry M <waltermccone88@gmail.com>
2021-01-20 20:01:19 -0600

    Added some psuedocode to the javascript

---
+++ 20-jQueryCalculator/Unsolved/javascript.js
@@ -0,0 +1,19 @@
+//create Dom for the global elements
+
+
+// create a function that builds an array to collect the numbers selected
+
+
+// create functions for addition
+
+// create a function for subtraction
+
+// create a function for multiplication
+
+// create a function for division
+
+// Create a function to store the answer in a variable
+
+//create a function that clears the answer 
+
+
